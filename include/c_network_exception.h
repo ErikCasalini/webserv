af76e2ed83424cdd77799e89622a9a085fdf9e36
ErikCasalini <153425818+ErikCasalini@users.noreply.github.com>
2026-01-15 13:44:50 +0100

    add: bind and listen, critical_exception now accepts strerror

--- include/c_network_exception.h
+++ include/c_network_exception.h
@@ -1,6 +1,11 @@
 #include <stdexcept>
 
-class critical_exception : public std::runtime_error {};
+class critical_exception : public std::runtime_error
+{
+	public:
+	critical_exception(const char* err)
+	: std::runtime_error(err) {};
+};
 
 class recoverable_exception : public std::runtime_error {};
 
--- src/lib/c_network_exception_wrapper.cpp
+++ src/lib/c_network_exception_wrapper.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <exception>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -7,6 +8,22 @@ int socket_ex(int domain, int type, int protocol)
 {
 	int fd = socket(domain, type, protocol);
 	if (fd == -1)
-		throw std::exception();
+		throw critical_exception(strerror(errno));
 	return (fd);
+}
+
+int	bind_ex(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
+{
+	int ret = bind(sockfd, addr, addrlen);
+	if (ret < 0)
+		throw critical_exception(strerror(errno));
+	return (ret);
+}
+
+int listen_ex(int sockfd, int backlog)
+{
+	int ret = listen(sockfd, backlog);
+	if (ret < 0)
+		throw critical_exception(strerror(errno));
+	return (ret);
 }
