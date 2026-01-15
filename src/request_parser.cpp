d794241cc071ee3e710afbe470a8bdf66b61cd15
jules <142106821+julesportier@users.noreply.github.com>
2026-01-15 17:27:02 +0100

    ref(request_parser): use CRLF macro in place of \r\n

--- src/request_parser.cpp
+++ src/request_parser.cpp
@@ -26,7 +26,7 @@ void Request::parse()
 {
 	size_t pos = 0;
 	// The rfc9112 specify that we SHOULD ignore at least one crlf prior to the request.
-	while (m_buffer.substr(pos, 2) == "\r\n")
+	while (m_buffer.substr(pos, 2) == CRLF)
 		pos += 2;
 	m_request.method = _Request::parse_method(m_buffer, pos);
 	m_request.target = _Request::parse_target(m_buffer, pos);
