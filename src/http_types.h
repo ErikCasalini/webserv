991d33529a3462a332c9b575fa265ce492263d22
jules <142106821+julesportier@users.noreply.github.com>
2026-01-15 07:18:22 +0100

    feat: some types to store http messages

---
+++ src/http_types.h
@@ -0,0 +1,32 @@
+#ifndef HTTP_TYPES_H
+# define HTTP_TYPES_H
+
+# include <map>
+# include <string>
+
+enum method_t
+{
+	del,
+	get,
+	post
+};
+
+enum protocol_t
+{
+	zero_nine,
+	one,
+	one_one,
+	two,
+	three
+};
+
+struct request_t
+{
+	method_t method;
+	std::string target;
+	protocol_t protocol;
+	std::map<std::string, std::string> header;
+	std::string body;
+};
+
+#endif
