65074c440fbe8edfea68f59e8686a0b3ad33aa13
Jules Portier <142106821+julesportier@users.noreply.github.com>
2026-01-23 11:57:25 +0100

    lib(cctype_cast): add string trimming functions

--- src/lib/cctype_cast.cpp
+++ src/lib/cctype_cast.cpp
@@ -1,4 +1,5 @@
 #include <cctype>
+#include "../../include/cctype_cast.h"
 
 int to_lowercase(char c)
 {
@@ -13,4 +14,29 @@ int is_graph(char c)
 int is_space(char c)
 {
 	return (std::isspace(static_cast<unsigned char>(c)));
+}
+
+void strltrim(std::string& str, int (*func)(char))
+{
+	size_t pos = 0;
+	while (func(str[pos]))
+		++pos;
+	str.erase(0, pos);
+}
+
+void strrtrim(std::string& str, int (*func)(char))
+{
+	size_t len = str.length();
+	if (len == 0)
+		return ;
+	size_t pos = len - 1;
+	while (pos > 0 && func(str[pos]))
+		--pos;
+	str.erase(pos + 1, len - pos);
+}
+
+void strtrim(std::string& str, int (*func)(char))
+{
+	strltrim(str, func);
+	strrtrim(str, func);
 }
