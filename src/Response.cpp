e155b7cc1d4c35b6c55a76d2fbab4fea0a247adb
ecasalin <ecasalin@42.student>
2026-02-04 09:15:32 +0100

    add: Response.extract_uri_elem()

--- src/Response.cpp
+++ src/Response.cpp
@@ -1,8 +1,14 @@
 #include "Response.hpp"
 #include <cstring>
+#include "http_types.h"
+
+std::string	authorized_chars("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~" // Unreserved
+							":/?[]@#" // Reserved gen-delims
+							"!$&'()*+,;=" // Reserved sub-delims
+							"%"); // Url encoding
 
 Response::Response(void)
-: m_sockfd(-1), m_buffer("TEST BUFFER")
+: m_sockfd(-1)
 {}
 
 Response::~Response(void)
@@ -31,6 +37,34 @@ size_t	Response::get_buf_size(void) const
 void	Response::clear(void)
 {
 	m_sockfd = -1;
-	m_request = request_t(); // a dire a Jules
+	m_request.clear();
 	m_buffer.clear();
 }
+
+void	extract_uri_elem(std::string&uri, std::string &path, std::string &querry)
+{
+	size_t		pos = uri.find('?');
+	size_t		end = std::string::npos;
+
+	path.assign(uri, 0, pos);
+	if (pos != std::string::npos) { // if querry present
+		pos++; // after '?'
+		end = uri.find(pos, '#');
+		if (end != std::string::npos)
+			querry.assign(uri, pos, end - pos); // querry is everything between ? and #
+		else
+			querry.assign(uri, pos); // querry is everything after '?' (or nothing if pos = m_request.target.size)
+	}
+}
+
+// void	Response::parse_uri(void)
+// {
+// 	if (!m_request.target.size()
+// 		|| m_request.target[0] != '/'
+// 		|| m_request.target.find_first_not_of(authorized_chars) != std::string::npos) {
+// 		m_request.status = bad_request;
+// 		return ;
+// 	}
+
+
+// }
--- src/Response.hpp
+++ src/Response.hpp
@@ -10,23 +10,29 @@ class Response
 
 public:
 
-					Response(void);
-					~Response(void);
-				// 	Response(const Response &src);
-	// Response		&operator=(const Response &rhs);
-	const request_t	&get_request(void) const;
-	void			set_request(const request_t &request);
-	void			clear(void);
-	const char		*get_buf(void) const;
-	size_t			get_buf_size(void) const;
+						Response(void);
+						~Response(void);
+				// 		Response(const Response &src);
+	// Response			&operator=(const Response &rhs);
+	const request_t		&get_request(void) const;
+	void				set_request(const request_t &request);
+	void				clear(void);
+	const char			*get_buf(void) const;
+	size_t				get_buf_size(void) const;
+	void				parse_uri(void);
 
-	int				m_sockfd;
+	int					m_sockfd;
+	static std::string	authorized_chars;
 
 private:
 
 	request_t		m_request;
 	std::string		m_buffer;
-
+	std::string		m_querry;
+	std::string		m_path;
+	status_t		m_status;
 };
+
+void	extract_uri_elem(std::string&uri, std::string &path, std::string &querry);
 
 #endif
