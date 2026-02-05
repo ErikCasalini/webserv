b00ae1d792f9b699699b4c1552290b20e4fa30ff
ecasalin <ecasalin@42.student>
2026-02-05 09:14:39 +0100

    test: add tests test_Response

--- test/test_Response.cpp
+++ test/test_Response.cpp
@@ -1,6 +1,8 @@
 #include "../src/Response.hpp"
 #include "lib_test.h"
 
+using namespace _Response;
+
 void	test_extract_uri_elem(void)
 {
 	std::string	uri("/src?querry");
@@ -16,6 +18,63 @@ void	test_extract_uri_elem(void)
 	extract_uri_elem(uri, path, querry);
 	assert((path == "/src"));
 	assert((querry == "querry"));
+
+	uri = "/src?querry#o";
+	path.clear();
+	querry.clear();
+	extract_uri_elem(uri, path, querry);
+	assert((path == "/src"));
+	assert((querry == "querry"));
+
+	uri = "/sr/c/?que#rry#";
+	path.clear();
+	querry.clear();
+	extract_uri_elem(uri, path, querry);
+	assert((path == "/sr/c/"));
+	assert((querry == "que"));
+
+	uri = "/src?";
+	path.clear();
+	querry.clear();
+	extract_uri_elem(uri, path, querry);
+	assert((path == "/src"));
+	assert((querry == ""));
+
+	uri = "/src####";
+	path.clear();
+	querry.clear();
+	extract_uri_elem(uri, path, querry);
+	assert((path == "/src"));
+	assert((querry == ""));
+
+	uri = "/?###";
+	path.clear();
+	querry.clear();
+	extract_uri_elem(uri, path, querry);
+	assert((path == "/"));
+	assert((querry == ""));
+
+	uri = "/###?ok";
+	path.clear();
+	querry.clear();
+	extract_uri_elem(uri, path, querry);
+	assert((path == "/"));
+	assert((querry == ""));
+
+	uri = "/?ok#?ok";
+	path.clear();
+	querry.clear();
+	extract_uri_elem(uri, path, querry);
+	assert((path == "/"));
+	assert((querry == "ok"));
+
+	uri = "/oui????";
+	path.clear();
+	querry.clear();
+	extract_uri_elem(uri, path, querry);
+	assert((path == "/oui"));
+	assert((querry == "???"));
+
 }
 
 int	main(void)
