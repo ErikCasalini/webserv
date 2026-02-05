6e48c6cc02be84277a855943f10157794a2d03d5
ecasalin <ecasalin@42.student>
2026-02-05 13:02:54 +0100

    test: add test_split_path()

--- test/test_Response.cpp
+++ test/test_Response.cpp
@@ -1,5 +1,6 @@
 #include "../src/Response.hpp"
 #include "lib_test.h"
+#include <list>
 
 using namespace _Response;
 
@@ -77,8 +78,53 @@ void	test_extract_uri_elem(void)
 
 }
 
+void	test_split_path(void)
+{
+	std::list<std::string>				segments;
+	std::list<std::string>::iterator	it;
+	segments = split_path("/test/");
+	it = segments.begin();
+	assert((segments.size() == 2));
+	assert((*it == "test"));
+	assert((*++it == ""));
+
+	segments = split_path("/test");
+	it = segments.begin();
+	assert((segments.size() == 1));
+	assert((*it == "test"));
+
+	segments = split_path("/test/deux/");
+	it = segments.begin();
+	assert((segments.size() == 3));
+	assert((*it == "test"));
+	assert((*(++it) == "deux"));
+	assert((*(++it) == ""));
+
+	segments = split_path("/test///");
+	it = segments.begin();
+	assert((segments.size() == 4));
+	assert((*(it) == "test"));
+	assert((*(++it) == ""));
+	assert((*(++it) == ""));
+	assert((*(++it) == ""));
+
+	segments = split_path("/test///./..///");
+	assert((segments.size() == 8));
+	it = segments.begin();
+	assert((*(it) == "test"));
+	assert((*(++it) == ""));
+	assert((*(++it) == ""));
+	assert((*(++it) == "."));
+	assert((*(++it) == ".."));
+	assert((*(++it) == ""));
+	assert((*(++it) == ""));
+	assert((*(++it) == ""));
+
+}
+
 int	main(void)
 {
 	test(test_extract_uri_elem, "test_extract_uri_elem");
+	test(test_split_path, "test_split_path");
 	return (0);
 }
