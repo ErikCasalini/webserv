4572b61cee1bce295e289b98a36f2a1dee638aff
ErikCasalini <153425818+ErikCasalini@users.noreply.github.com>
2026-02-06 15:21:28 +0100

    test: add test_url_decode() test_decode_segments()

--- test/test_Response.cpp
+++ test/test_Response.cpp
@@ -93,6 +93,11 @@ void	test_split_path(void)
 	assert((segments.size() == 1));
 	assert((*it == "test"));
 
+	segments = split_path("/");
+	it = segments.begin();
+	assert((segments.size() == 1));
+	assert((*it == ""));
+
 	segments = split_path("/test/deux/");
 	it = segments.begin();
 	assert((segments.size() == 3));
@@ -122,9 +127,141 @@ void	test_split_path(void)
 
 }
 
+void	test_url_decode(void)
+{
+	std::string	url_code;
+
+	url_code = "12";
+	assert((url_decode(url_code) == 18));
+
+	url_code = "02";
+	assert((url_decode(url_code) == 2));
+
+	url_code = "FF";
+	assert((url_decode(url_code) == 255));
+
+	url_code = "aa";
+	assert((url_decode(url_code) == 170));
+
+	try {
+		url_code = "00";
+		url_decode(url_code);
+		assert((false));
+	} catch (std::invalid_argument &e) {}
+
+	try {
+		url_code = "2F";
+		url_decode(url_code);
+		assert((false));
+	} catch (std::invalid_argument &e) {}
+
+	try {
+		url_code = "5C";
+		url_decode(url_code);
+		assert((false));
+	} catch (std::invalid_argument &e) {}
+
+	try {
+		url_code = "-5";
+		url_decode(url_code);
+		assert((false));
+	} catch (std::invalid_argument &e) {}
+
+	try {
+		url_code = "F%";
+		url_decode(url_code);
+		assert((false));
+	} catch (std::invalid_argument &e) {}
+
+}
+
+void	test_decode_segments(void)
+{
+	std::list<std::string>				segments;
+	std::list<std::string>::iterator	it;
+
+	segments.push_back("src");
+	segments.push_back("src%75"); // u
+	segments.push_back("%21%24"); // ! $
+	segments.push_back("src%2ASRC%2E"); // * .
+	segments.push_back("src%2ASRC%2Esrc"); // * .
+	segments.push_back("%30"); // 0
+	segments.push_back("");
+	segments.push_back("");
+	decode_segments(segments);
+	it = segments.begin();
+	assert((segments.size() == 8));
+	assert((*it == "src"));
+	assert((*(++it) == "srcu"));
+	assert((*(++it) == "!$"));
+	assert((*(++it) == "src*SRC."));
+	assert((*(++it) == "src*SRC.src"));
+	assert((*(++it) == "0"));
+	assert((*(++it) == ""));
+	assert((*(++it) == ""));
+
+	segments.clear();
+	segments.push_back("%");
+	try {
+		decode_segments(segments);
+		assert(false);
+	} catch (std::invalid_argument &e) {}
+
+	segments.clear();
+	segments.push_back("%0");
+	try {
+		decode_segments(segments);
+		assert(false);
+	} catch (std::invalid_argument &e) {}
+
+	segments.clear();
+	segments.push_back("src%");
+	try {
+		decode_segments(segments);
+		assert(false);
+	} catch (std::invalid_argument &e) {}
+
+	segments.clear();
+	segments.push_back("src%1");
+	try {
+		decode_segments(segments);
+		assert(false);
+	} catch (std::invalid_argument &e) {}
+
+	segments.clear();
+	segments.push_back("%1g");
+	try {
+		decode_segments(segments);
+		assert(false);
+	} catch (std::invalid_argument &e) {}
+
+	segments.clear();
+	segments.push_back("%xsrc");
+	try {
+		decode_segments(segments);
+		assert(false);
+	} catch (std::invalid_argument &e) {}
+
+	segments.clear();
+	segments.push_back("src%FF%%%%%%");
+	try {
+		decode_segments(segments);
+		assert(false);
+	} catch (std::invalid_argument &e) {}
+
+	segments.clear();
+	segments.push_back("0%");
+	try {
+		decode_segments(segments);
+		assert(false);
+	} catch (std::invalid_argument &e) {}
+}
+
 int	main(void)
 {
 	test(test_extract_uri_elem, "test_extract_uri_elem");
 	test(test_split_path, "test_split_path");
+	test(test_url_decode, "test_url_decode");
+	test(test_decode_segments, "test_decode_segments");
 	return (0);
 }
