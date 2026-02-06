92989909e5a94d27e3cb961edb705d14d97e69ca
ErikCasalini <153425818+ErikCasalini@users.noreply.github.com>
2026-02-06 16:01:24 +0100

    add: Response::decode_segments

--- src/Response.hpp
+++ src/Response.hpp
@@ -38,8 +38,11 @@ private:
 
 namespace _Response
 {
-	void						extract_uri_elem(std::string&uri, std::string &path, std::string &querry);
-	std::list<std::string>		split_path(const std::string &path);
+	void					extract_uri_elem(std::string&uri, std::string &path, std::string &querry);
+	std::list<std::string>	split_path(const std::string &path);
+	unsigned char			url_decode(const std::string &url_code);
+	void					decode_segments(std::list<std::string> &segments);
+	std::string				create_path(std::list<std::string> &segments);
 }
 
 #endif
