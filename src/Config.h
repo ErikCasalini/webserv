2341e8739d7856342e3cbea8bfaf4bdb6a670939
ecasalin <ecasalin@42.student>
2026-02-11 09:26:28 +0100

    ref: Config.h

--- src/Config.h
+++ src/Config.h
@@ -11,23 +11,44 @@ using std::string;
 using std::map;
 using std::vector;
 
+struct main_t {
+	events_t events;
+	http_t http;
+};
+
 struct events_t {
 	unsigned int worker_connections = DEFAULT_WORKER_CONNECTIONS;
+};
+
+struct http_t {
+	// map<string, string> types;
+	// string default_type;
+	vector<server_t> server;
+	autoindex_t autoindex;
+	error_page_t error_page;
+	unsigned int max_body;
+	unsigned int keepalive_timeout;
+};
+
+struct server_t {
+	vector<listen_t> listen;
+	location_t location;
+	error_page_t error_page;
+	autoindex_t autoindex;
+	redirection_t redirection;
+};
+
+struct location_params_t {
+	limit_except_t limit_except_t;
+	autoindex_t autoindex;
+	error_page_t error_page;
+	string index;
+	redirection_t redirection;
 };
 
 struct listen_t {
 	uint32_t ip;
 	uint16_t port;
-};
-
-struct location_t {
-	string root;
-	string index;
-};
-
-struct error_page_t {
-	vector<int> status;
-	string target;
 };
 
 struct autoindex_t {
@@ -38,30 +59,8 @@ struct limit_except_t {
 	vector<string> methods;
 };
 
-struct server_t {
-	listen_t listen;
-	location_t location;
-	error_page_t error_page;
-	autoindex_t autoindex;
-	limit_except_t limit_except;
-};
-
-struct http_t {
-	map<string, string> types;
-	string default_type;
-	server_t server;
-	autoindex_t autoindex;
-	limit_except_t limit_except;
-	unsigned int keepalive_timeout;
-};
-
-struct config_t {
-	events_t events;
-	http_t http;
-};
-
-// very long access
-// config.http.server.listen.ip;
-// config.http.server.listen.port;
+typedef std::multimap<int, string> redirection_t;
+typedef std::multimap<string, location_params_t> location_t;
+typedef std::multimap<int, string> error_page_t;
 
 #endif
