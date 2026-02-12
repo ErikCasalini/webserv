7d15def99e915c5890790a964cc764bc4d4875ff
Erik <153425818+ErikCasalini@users.noreply.github.com>
2026-02-12 19:34:23 +0100

    ref: minor changes to Config.h

--- src/Config.h
+++ src/Config.h
@@ -5,7 +5,7 @@
 # include <string>
 # include <vector>
 
-# define DEFAULT_WORKER_CONNECTIONS 512
+# define DEFAULT_max_connECTIONS 512
 
 using std::string;
 using std::map;
@@ -20,17 +20,13 @@ struct listen_t {
 	u_int16_t port;
 };
 
-struct autoindex_t {
-	bool on;
-};
-
 struct limit_except_t {
 	vector<string> methods;
 };
 
 struct location_params_t {
 	limit_except_t limit_except;
-	autoindex_t autoindex;
+	bool autoindex;
 	error_page_t error_page;
 	string index;
 	redirection_t redirection;
@@ -42,7 +38,7 @@ struct server_t {
 	vector<listen_t> listen;
 	location_t location;
 	error_page_t error_page;
-	autoindex_t autoindex;
+	bool autoindex;
 	redirection_t redirection;
 };
 
@@ -50,14 +46,14 @@ struct http_t {
 	// map<string, string> types;
 	// string default_type;
 	vector<server_t> server;
-	autoindex_t autoindex;
+	bool autoindex;
 	error_page_t error_page;
 	unsigned int max_body;
 	unsigned int keepalive_timeout;
 };
 
 struct events_t {
-	unsigned int worker_connections;
+	unsigned int max_connections;
 };
 
 struct config_t {
